d261af424ad73dc139fbcdcc446f3992e5ea1b61
Aaron Conway <aaronconwayphd@gmail.com>
2020-06-10 10:44:17 -0400

    selection column nas fixed

--- R/create_clean_rob.R
+++ R/create_clean_rob.R
@@ -6,11 +6,17 @@
 ##' @export
 create_clean_rob <- function(rob) {
 
-    col_names <- names(read_csv("data/review_69353_quality_assessment_export.csv", n_max = 0))
-   rob <-  read_csv("data/review_69353_quality_assessment_export.csv", col_names = col_names, skip = 3)
+    col_names <- names(read_csv(rob, 
+                                n_max = 0))
+   rob <-  read_csv(rob, 
+                    col_names = col_names, skip = 3)
 rob %>% 
   filter(Reviewer == "Consensus") %>% 
   select("study" = `Study Id`,
          contains("overall"),
-         contains("bland"))
+         contains("bland")) %>% 
+  mutate(`What was the overall risk of bias associated with the selection of participants?`=
+           ifelse(is.na(`What was the overall risk of bias associated with the selection of participants?`),
+                  rob$`What was the overall risk of bias associated with the selection of participants? for All outcomes`,
+         `What was the overall risk of bias associated with the selection of participants?`))
 }
